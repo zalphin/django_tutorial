66af5fde1e72ddd8e4c0a41ef8091f1890926bae
zalphin <zalphin@gmail.com>
2024-08-20 16:54:37 -0600

    Initial commit for django tutorial app

---
+++ posts/urls.py
@@ -0,0 +1,9 @@
+# posts/urls.py
+from django.urls import path
+
+from .views import PostListView, PostDetailView
+
+urlpatterns = [
+    path("<int:pk>/", PostDetailView.as_view(), name="post_detail"),
+    path('', PostListView.as_view(), name="post_list"),
+]
