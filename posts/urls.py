26481523154e60687d7251cc17e02efa414f2192
zalphin <zalphin@gmail.com>
2024-08-26 16:33:58 -0600

    Formalized Flow and used generic views

--- posts/urls.py
+++ posts/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 from .views import PostListView, PostDetailView
 
+app_name = 'posts'
 urlpatterns = [
     path("<int:pk>/", PostDetailView.as_view(), name="post_detail"),
     path('', PostListView.as_view(), name="post_list"),
