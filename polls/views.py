6722043adf1ba022adc81d18534ae0c80869ed8e
zalphin <zalphin@gmail.com>
2024-08-28 16:32:48 -0600

    Fleshed out testing for polls app

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.db.models import F
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -13,28 +14,24 @@ class IndexView(generic.ListView):
 
         def get_queryset(self):
                 """ Return the last 5 published questions. """
-                return Question.objects.order_by("-pub_date")[:5]
+                return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
         
 class DetailView(generic.DetailView):
         model = Question
         template_name = "polls/detail.html"
 
+        def get_queryset(self):
+                """
+                Excludes any questions that aren't published yet.
+                """
+                return Question.objects.filter(pub_date__lte=timezone.now())
+
 class ResultsView(generic.DetailView):
         model = Question
         template_name = "polls/results.html"
 
-# def index(request):
-#         latest_question_list = Question.objects.order_by("-pub_date")[:5]
-#         context = {"latest_question_list": latest_question_list}
-#         return render(request, "polls/index.html", context)
-
-# def detail(request, question_id):
-#         question = get_object_or_404(Question, pk=question_id)
-#         return render(request, "polls/detail.html", {"question": question})
-
-# def results(request, question_id):
-#         question = get_object_or_404(Question, pk=question_id)
-#         return render(request, "polls/results.html", {"question": question})
+        def get_queryset(self):
+                return Question.objects.filter(pub_date__lte=timezone.now())
 
 def vote(request, question_id):
         question = get_object_or_404(Question, pk=question_id)
