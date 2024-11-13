ef5a66b219aba2944d87acb2111a5df99d42182e
zalphin <zalphin@gmail.com>
2024-11-12 17:06:53 -0700

    added more responses and optional/required responses

--- polls/forms.py
+++ polls/forms.py
@@ -3,6 +3,9 @@ from django import forms
 class CreateNewPollForm(forms.Form):
     question_text = forms.CharField(label="Question:", required=True)
     pub_date = forms.DateField(help_text="When should the poll start?")
-    response_1 = forms.CharField()
-    response_2 = forms.CharField()
-    response_3 = forms.CharField()
+    response_1 = forms.CharField(label="Response 1*:", required=True)
+    response_2 = forms.CharField(label="Repsonse 2*:")
+    response_3 = forms.CharField(required=False)
+    response_4 = forms.CharField(required=False)
+    response_5 = forms.CharField(required=False)
+    response_6 = forms.CharField(required=False)
